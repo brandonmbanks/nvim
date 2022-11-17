c1d70fca617fee5f16dc58e12ff9dcc4ff92281d
Brandon Banks <10030196+brandonmbanks@users.noreply.github.com>
2022-11-16 19:52:13 -0500

    put options in map

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,49 +1,49 @@
 -- :help options
-vim.opt.backup = false              -- disables backup file when writing a file
-vim.opt.swapfile = false            -- disables swap file
-vim.opt.undofile = true             -- unables persistent undo by saving undo history
+local options = {
+    backup = false,             -- disables backup file when writing a file
+    swapfile = false,           -- disables swap file
+    undofile = true,            -- enables persistent undo by saving undo history
+    spell = true,               -- enables vim spellchecker
+    spelllang='en_us',          -- sets language to us English
+    timeoutlen = 1000,          -- max time to wait for mapped sequence to complete
+    clipboard = "unnamedplus",  -- allows nvim to access system clipboard
+    cmdheight = 1,              -- cmd line height
+    pumheight = 10,             -- pop up menu height
+    signcolumn = "yes",         -- always display signcolumn to not move text
+    showmode = false,           -- disable vim mode like -- INSERT --
+    splitbelow = true,          -- splits dont move current window
+    splitright = true,          -- vsplits dont move current window
+    titlestring = '%t',         -- sets title to current file
+    title = true,               -- enables title
+    -- line numbers
+    number = true,              -- display line numbers
+    relativenumber = true,      -- display relative line numbers
+    cursorline = true,          -- highlights the cursor line
+    cursorcolumn = true,        -- highlights the cursor column
+    mouse = 'a',                -- adds mouse support, i.e. clicking moves the cursor
+    scrolloff = 8,              -- minimum number of lines to keep above and below cursor
+    sidescrolloff = 8,          -- number number of columns to keep right and left of cursor
+    confirm = true,             -- confirm saving changes when quitting a file without saving
+    fileencoding = "utf-8",
+    wrap = false,               -- displays lines as one long line
+    tabstop = 4,                -- number of spaces that a tab in a file counts for
+    softtabstop = 4,            -- number of spaces inserted for a tab while editing
+    shiftwidth = 4,             -- number of spaces to use for each step of indent
+    expandtab = true,           -- converts tabs to spaces
+    smartindent = true,         -- smart indent when starting a new line
+    -- searching
+    hlsearch = true,            -- highlight previous search pattern
+    incsearch = true,           -- show where the search pattern matches as its typed
+    ignorecase = true,          -- ignores case in search pattern
+    smartcase = true,           -- overrides ignorecase if search pattern contains upper case
+    termguicolors = true,
 
-vim.opt.timeoutlen = 1000           -- max time to wait for mapped sequence to complete
+    completeopt = "menu,menuone,noselect",                      -- autocomplete options
+    guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20",    -- default cursor setttings
+    guifont = "monospace:h17",                                  -- font to use in gui apps
+}
 
-vim.opt.clipboard = "unnamedplus"   -- allows nvim to access system clipboard
-
-vim.opt.cmdheight = 1               -- cmd line height
-vim.opt.pumheight = 10              -- pop up menu height
-vim.opt.signcolumn = "yes"          -- always display signcolumn to not move text
-vim.opt.showmode = false            -- disable vim mode like -- INSERT --
-
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
--- vim.opt.iskeyword:append "-"        -- makes a word like foo-bar one word
-
--- line numbers
-vim.opt.number = true               -- display line numbers
-vim.opt.relativenumber = true       -- display relative line numbers
-vim.opt.cursorline = true           -- highlights the cursor line
-vim.opt.cursorcolumn = true         -- highlights the cursor column
-
-vim.opt.scrolloff = 8               -- minimum number of lines to keep above and below cursor
-vim.opt.sidescrolloff = 8           -- number number of columns to keep right and left of cursor
-
-vim.opt.fileencoding = "utf-8"
-
-vim.opt.wrap = false                -- displays lines as one long line
-vim.opt.tabstop = 4                 -- number of spaces that a tab in a file counts for
-vim.opt.softtabstop = 4             -- number of spaces inserted for a tab while editing
-vim.opt.shiftwidth = 4              -- number of spaces to use for each step of indent
-vim.opt.expandtab = true            -- converts tabs to spaces
-vim.opt.smartindent = true          -- smart indent when starting a new line
-
--- searching
-vim.opt.hlsearch = true             -- highlight previous search pattern
-vim.opt.incsearch = true            -- show where the search pattern matches as its typed
-vim.opt.ignorecase = true           -- ingores case in search pattern
-vim.opt.smartcase = true            -- overrides ignorecase if search pattern contains upper case
-
-vim.opt.guicursor = ""
-vim.opt.guifont = "monospace:h17"   -- font to use in gui apps
-vim.opt.termguicolors = true
-
-vim.opt.completeopt="menu,menuone,noselect"
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
 
